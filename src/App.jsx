ad3f2803b0f2cfef514e5200abcec0c7808acbf2
Sushant <khanalsushan14@gmail.com>
2026-05-18 14:00:32 +0545

    created basic structure and grid of the game along with the css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
+import { GameHeader } from "./components/GameHeader";
+import { Card } from "./components/Card";
+
+const cardValues = [
+  "🍎",
+  "🍌",
+  "🍇",
+  "🍊",
+  "🍓",
+  "🍑",
+  "🍍",
+  "🥝",
+  "🍎",
+  "🍌",
+  "🍇",
+  "🍊",
+  "🍓",
+  "🍑",
+  "🍍",
+  "🥝",
+];
 
 
 function App() {
   
-  return (
-    <>
-    <h1>Memory Game</h1>
-    </>
-  )
+  return( 
+    <div className="App">
+      <GameHeader score={0} moves={0} onReset={() => {}} />
+
+        <div className="cards-grid">
+          {cardValues.map((card) => (
+            <Card card={card} />
+          ))}
+        </div>  
+
+    </div>
+  );
 }
 
 export default App
